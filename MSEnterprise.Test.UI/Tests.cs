797c1e3f542af838bf338fa2c7f9b19e4b30e413
Wennder dos Santos <wennder.santos@outlook.com>
2016-08-06 19:44:28 -0300

    Adicionado solution e projeto ui test

---
+++ MSEnterprise.Test.UI/Tests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+using Xamarin.UITest.Android;
+
+namespace MSEnterprise.Test.UI
+{
+    [TestFixture]
+    public class Tests
+    {
+        AndroidApp app;
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = ConfigureApp
+                .Android
+                .ApkFile ("../ms-enterprise/platforms/android/build/outputs/apk/android-debug.apk")
+                .StartApp();
+        }
+
+        [Test]
+        public void AppLaunches()
+        {
+            app.Screenshot("First screen.");
+        }
+    }
+}
+
