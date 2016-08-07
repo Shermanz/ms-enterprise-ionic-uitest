553acc65b929ad50b8a345ff6b43116b3e28f8ec
Mahmoud Ali <mahmoud.ali@lambda3.com.br>
2016-08-06 22:30:50 -0300

    Remove sass

--- ms-enterprise/gulpfile.js
+++ ms-enterprise/gulpfile.js
@@ -1,30 +1,17 @@
 /// <binding BeforeBuild='default' />
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-var bower = require('bower');
+var bower = require('gulp-bower');
 var concat = require('gulp-concat');
-var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
 
 var paths = {
-  sass: ['./scss/**/*.scss']
 };
 
-gulp.task('default', ['sass']);
+gulp.task('default', ['bower']);
 
-gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
-    .pipe(sass())
-    .on('error', sass.logError)
-    .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
-    .on('end', done);
+gulp.task('bower', function () {
+    return bower();
 });
-
-
